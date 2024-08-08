11f16db19489ddbcaa357bd882f837f004469aaa
Julie Steiner <jul.stein@hotmail.com>
2024-08-08 21:31:34 +0200

    Son background et étoile

--- Assets/Script Collectible étoile collectable.cs
+++ Assets/Script Collectible étoile collectable.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
+    public AudioClip collectSound; // Ajoutez cette ligne pour le son de collecte
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -10,6 +12,10 @@ public class Collectible : MonoBehaviour
             if (playerInventory != null)
             {
                 playerInventory.AddCollectible();
+
+                 // Jouer le son de collecte
+                AudioSource.PlayClipAtPoint(collectSound, transform.position);
+                
                 Destroy(gameObject); // Détruit l'objet une fois collecté
             }
         }
