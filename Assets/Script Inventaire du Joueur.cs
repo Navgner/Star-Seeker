5b2efaa7bbe7367e0a74f631ca04ab331d33f439
Navgner <rengvan@hotmail.ch>
2024-08-16 23:26:51 +0200

    fini?

--- Assets/Script Inventaire du Joueur.cs
+++ Assets/Script Inventaire du Joueur.cs
@@ -21,15 +21,15 @@ public class PlayerInventory : MonoBehaviour
 
         if (collectedCount == 1)
         {
-            dialogueManager.ShowDialogue("Oh, je reconnais cette étoile...");
+            dialogueManager.ShowDialogue("Oh, je reconnais cette étoile, elle appartient à Ladon ! Le dragon céleste de Héra, ancien gardien des jardins des Hespérides ! Mmmmmh… Les autres ne doivent pas être loin…");
         }
         else if (collectedCount == 6)
         {
-            dialogueManager.ShowDialogue("Elles sont vraiment belles...");
+            dialogueManager.ShowDialogue("Elles sont vraiment belles ces étoiles, si brillantes qu'elles me rappellent les pommes dorées des jardins, celles qu'Hercule parvint à voler sous le nez du dragon.");
         }
         else if (collectedCount == 12)
         {
-            dialogueManager.ShowDialogue("Oh c’est Eltanin...");
+            dialogueManager.ShowDialogue("Oh c’est Eltanin, la plus lumineuse sans aucun doute ! Ladon peut se réveiller et faire dissiper ces ténèbres dès que j'ai récolté le reste des étoiles.");
         }
         else if (collectedCount == totalCollectibles)
         {
